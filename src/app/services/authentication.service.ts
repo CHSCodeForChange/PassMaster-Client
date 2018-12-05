17b55bb68bc322caec3783cfad0ceda552f61f47
computerquest <jaredstigter@gmail.com>
2018-12-05 08:22:09 -0500

    got the students to load

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {map, tap} from 'rxjs/operators';
 
-
+//this might only be able to store one user in local server memory
 @Injectable()
 export class AuthenticationService {
   url = 'https://passmaster.herokuapp.com/rest/login/';
@@ -15,6 +15,7 @@ export class AuthenticationService {
       .pipe(map((res: Response) => {
         // login successful if there's a jwt token in the response
         if (res['token']) {
+          console.log('we are putting up ',res['token']);
           // store user details and jwt token in local storage to keep user logged in between page refreshes
           localStorage.setItem('Authorization', 'Token ' + res['token']);
         }
